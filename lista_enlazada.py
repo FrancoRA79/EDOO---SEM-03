f50c6d439ae7faac775a8f91e2088d519088e4f4
FrancoRA79 <cfreyesa@unitru.edu.pe>
2026-09-11 19:45:00 -0500

    Agrego función de busqueda

--- lista_enlazada.py
+++ lista_enlazada.py
@@ -1,1 +1,8 @@
 print("Uso de la listas simplemente enlazadas")
+
+def buscar_producto(productos, nombre):
+    for producto in productos:
+        if producto["nombre"] == nombre:
+            return producto
+
+    return None
